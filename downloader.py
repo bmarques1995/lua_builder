aa45d808464de4da36cbdcaef7eca00893c4a9ea
bmarques1995 <testebash@outlook.com>
2025-08-03 22:20:49 -0300

    Improved downloader

--- downloader.py
+++ downloader.py
@@ -2,21 +2,22 @@ import subprocess
 import sys
 import tarfile
 import urllib.request as request
+import os
 
 def main():
     download_url = sys.argv[1]
-    output_dir = sys.argv[2]
-    tar_file_path = f'{output_dir}/lua.tar.gz'
-    print("Scope")
-    print(tar_file_path)
-    print("End scope")
+    output_dir = os.path.normpath(sys.argv[2])
+    tar_file_path = os.path.normpath(f'{output_dir}/lua.tar.gz')
+    version_info = sys.version_info
     req = request.Request(download_url)
     with request.urlopen(req) as response:
         with open(tar_file_path, 'wb') as out_file:
             out_file.write(response.read())
     tar = tarfile.open(tar_file_path, 'r:gz')
-    tar.extractall(path=output_dir)
-    
+    if version_info.major < 3 or (version_info.major == 3 and version_info.minor < 12):
+        tar.extractall(path=output_dir)
+    else:
+        tar.extractall(path=output_dir, filter='fully_trusted')
 
 if __name__ == "__main__":
     try:
